2b6ded7ce1adc4b110a32fc961b09aa44863c01f
Aly Chavez <110579112+alychavez12@users.noreply.github.com>
2023-04-05 15:17:14 -0400

    adds a create, update and delte route working

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 
 
@@ -16,3 +17,18 @@ def finches_index(request):
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
     return render(request, "finches/detail.html", {"finch": finch})
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+    template_name = 'finches/finches_form.html'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['breed', 'description', 'age']    
+    template_name = 'finches/finches_form.html'
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches/'
+    template_name = 'finches/finches_confirm_delete.html'
