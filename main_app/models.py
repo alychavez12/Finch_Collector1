708f8933bfc7414fc2c5e4eb2584467514491cde
Aly Chavez <110579112+alychavez12@users.noreply.github.com>
2023-04-04 22:29:10 -0400

    adds a finch data migration list

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Finch(models.Model):
+    name = models.CharField(max_length=20) # varchar
+    breed = models.CharField(max_length=20) # varchar
+    description = models.TextField(max_length=250) # text data type
+    age = models.IntegerField(default=0)
+
+    def __str__(self): 
+        return self.name
